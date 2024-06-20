e79203ee3f20248611b2996d0c45fb0718358f47
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-06-20 20:29:04 +0530

    added results page

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,20 +2,28 @@ import React from "react";
 import "./SearchPage.css";
 import { useParams } from "react-router-dom";
 import useGoogleSearch from "../Custom Hooks/useGoogleSearch";
-import response from "../response";
+// import response from "../response";
 import { Link } from "react-router-dom";
 import logo from "../assets/google-logo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import Search from "./Search";
-import { Description, Image, LocalOffer, MoreVert, Room } from "@mui/icons-material";
+import {
+  Description,
+  Image,
+  LocalOffer,
+  MoreVert,
+  Room,
+} from "@mui/icons-material";
 
 const SearchPage = () => {
   const params = useParams();
   const { searchId } = params;
   // LIVE API CALL
-  // const { data } = useGoogleSearch(searchId);
-  const data = response;
-  console.log(data);
+  const { data } = useGoogleSearch(searchId);
+  // MOCK API CALL 
+  // const data = response;
+
+  // console.log(data.items.displayLink);
 
   return (
     <div className="searchPage">
@@ -27,11 +35,6 @@ const SearchPage = () => {
           <Search hideButtons />
           <div className="searchPage__options">
             <div className="searchPage__optionLeft">
-
-
-
-
-
               <div className="searchPage__option">
                 <SearchIcon />
                 <Link to="/all">All</Link>
@@ -56,14 +59,9 @@ const SearchPage = () => {
                 <MoreVert />
                 <Link to="/more">more</Link>
               </div>
-
-
-
-
-
             </div>
             <div className="searchPage__optionRight">
-            <div className="searchPage__option">
+              <div className="searchPage__option">
                 <Link to="/tools">Tools</Link>
               </div>
               <div className="searchPage__option">
@@ -74,7 +72,37 @@ const SearchPage = () => {
         </div>
       </div>
 
-      <div className="searchPage__results"></div>
+      {searchId && (
+        <div className="searchPage__results">
+          {/* adding how many results are displayed para  */}
+          <p className="searchPage__resultCount">
+            {/* by using the below method we can make sure if the data is not there then it wont show and error instead it will return undefined */}
+            About {data?.searchInformation.formattedTotalResults} results (
+            {data?.searchInformation.formattedSearchTime} seconds) for{" "}
+            {searchId}
+          </p>
+          {/* adding the list of result links  */}
+
+          {data?.items.map((item) => (
+            <div className="searchPage__result">
+              <a href={item.link}>
+                {item.pagemap?.cse_image?.length>0 && item.pagemap?.cse_image[0].src&&(
+                  <img 
+                  className="searchPage__resultImage"
+                  src={item.pagemap?.cse_image[0].src}
+                  alt={item.title}
+                />
+                )}
+                
+                {item.displayLink}</a>
+              <a className="searchPage__resultTitle" href={item.link}>
+                <h2>{item.title} </h2>
+              </a>
+              <p className="searchPage__resultSnippet">{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
